949e48cefdb8a4510229b21ac155a6526c58ae37
René Pacios <rene15009@hotmail.com>
2018-10-12 19:20:00 +0200

    Remove unused usings

--- src/Rene.Utils.Core/Extensions/StringExtensions.cs
+++ src/Rene.Utils.Core/Extensions/StringExtensions.cs
@@ -6,11 +6,7 @@
  *
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
 namespace System
