1285589fc829b2ccaaf39c857eb95694f8eeab16
René Pacios <rene15009@hotmail.com>
2019-05-20 22:58:12 +0200

    Added some simpleCache Unit Test and Fixed some bugs

--- src/Rene.Utils.Core/SimpleCache/SimpleCacheEngine.cs
+++ src/Rene.Utils.Core/SimpleCache/SimpleCacheEngine.cs
@@ -34,36 +34,42 @@ namespace Rene.Utils.Core.SimpleCache
             _cache[key] = new CacheItem<TValue>(value, expiresAfter);
         }
 
-        public TValue Get(TKey key)
-        {
-            if (!_cache.ContainsKey(key)) return default(TValue);
-
-
-            if (!_cache.TryGetValue(key, out var cached)) return default(TValue);
-
-
-            if (DateTimeOffset.Now - cached.Created >= cached.ExpiresAfter)
-            {
-                _cache.Remove(key);
-                return default(TValue);
-            }
-
-            return cached.Value;
-
-
-        }
+        public TValue Get(TKey key) => TryGetValue(key, out var cacheEntry) ? cacheEntry : default(TValue);
 
         public TValue GetOrCreate(TKey key, Func<TValue> createItem, TimeSpan? expiresAfter = null)
         {
-            if (_cache.TryGetValue(key, out var cached)) return cached.Value;
-            
-            // Key not in cache, so get data.
-            var cacheEntry = createItem();
+//            if (_cache.TryGetValue(key, out var cached)) return cached.Value;
+            if (TryGetValue(key, out var cacheEntry)) return cacheEntry;
 
-            Store(key, createItem(), expiresAfter ?? _defaultExpiresTime);
+            // Key not in cache, so get data.
+            cacheEntry = createItem();
+
+            Store(key, cacheEntry, expiresAfter ?? _defaultExpiresTime);
 
             return cacheEntry;
         }
+
+        private bool TryGetValue(TKey key, out TValue cacheValue)
+        {
+            if (!_cache.TryGetValue(key, out var cached))
+            {
+                cacheValue= default(TValue);
+                return false;
+            }
+
+
+          
+            if (DateTimeOffset.Now - cached.Created >= cached.ExpiresAfter)
+            {
+                _cache.Remove(key);
+                cacheValue = default(TValue);
+                return false;
+            }
+
+            cacheValue = cached.Value;
+            return true;
+        }
+
     }
 }
 
--- tests/Rene.Utils.Core.UnitTest/SimpleCache/SimpleCacheEngineTest.cs
+++ tests/Rene.Utils.Core.UnitTest/SimpleCache/SimpleCacheEngineTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rene.Utils.Core.SimpleCache;
+using Xunit;
 
 namespace Rene.Utils.Core.UnitTest.SimpleCache
 {
@@ -15,6 +16,53 @@ namespace Rene.Utils.Core.UnitTest.SimpleCache
         {
             _cache= new SimpleCacheEngine<string,string>(TimeSpan.FromMilliseconds(10));
         }
+
+        [Fact]
+        public void Insert_Item_Cache()
+        {
+            string key = nameof(Insert_Item_Cache);
+            string expected = "Hello World!!";
+            _cache.Store(key,expected,TimeSpan.FromSeconds(1));
+            Assert.Equal(expected, _cache.Get(key));
+        }
+
+        [Fact]
+        public void Insert_Item_Cache_If_Expired()
+        {
+            string key = nameof(Insert_Item_Cache);
+
+            string expected1 = "Hello World!!";
+            string expected2 = Guid.NewGuid().ToString();
+
+            var expire = TimeSpan.FromMilliseconds(150);
+
+            _cache.Store(key, expected1, expire);
+
+            var res = _cache.GetOrCreate(key, () => expected2, expire);
+            Assert.Equal(expected1, res);
+
+            System.Threading.Thread.Sleep(expire.Add(TimeSpan.FromMilliseconds(1)));
+
+            var res2 = _cache.GetOrCreate(key, () => expected2, expire);
+            Assert.NotEqual(res, res2);
+            Assert.Equal(expected2, res2);
+
+
+
+        }
+
+
+        [Fact]
+        public void Expired_Item_Cache()
+        {
+            string key = nameof(Expired_Item_Cache);
+            string expected = "Hello World!!";
+            _cache.Store(key, expected, TimeSpan.FromMilliseconds(150));
+            System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(200));
+            Assert.Equal(default(string), _cache.Get(key));
+        }
+
+
 
 
         public void Dispose()
