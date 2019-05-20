d7833781af90ac60f4d98d964ea92e927bfa179e
René Pacios <rene15009@hotmail.com>
2019-05-20 22:08:10 +0200

    Started Simple Cache engine

---
+++ src/Rene.Utils.Core/SimpleCache/SimpleCacheEngine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rene.Utils.Core.SimpleCache
+{
+
+    public class SimpleCacheEngine<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, CacheItem<TValue>> _cache = new Dictionary<TKey, CacheItem<TValue>>();
+
+        private readonly TimeSpan _defaultExpiresTime;
+
+        /// <summary>
+        /// Initialize Cache Engine with 60 seconds of expiration time
+        /// </summary>
+        public SimpleCacheEngine()
+        {
+            _defaultExpiresTime = TimeSpan.FromSeconds(60);
+        }
+
+        public SimpleCacheEngine(TimeSpan defaultExpiresTime)
+        {
+            _defaultExpiresTime = defaultExpiresTime;
+        }
+
+
+        public void Store(TKey key, TValue value)
+        {
+            Store(key, value, _defaultExpiresTime);
+        }
+
+        public void Store(TKey key, TValue value, TimeSpan expiresAfter)
+        {
+            _cache[key] = new CacheItem<TValue>(value, expiresAfter);
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (!_cache.ContainsKey(key)) return default(TValue);
+
+
+            if (!_cache.TryGetValue(key, out var cached)) return default(TValue);
+
+
+            if (DateTimeOffset.Now - cached.Created >= cached.ExpiresAfter)
+            {
+                _cache.Remove(key);
+                return default(TValue);
+            }
+
+            return cached.Value;
+
+
+        }
+
+        public TValue GetOrCreate(TKey key, Func<TValue> createItem, TimeSpan? expiresAfter = null)
+        {
+            if (_cache.TryGetValue(key, out var cached)) return cached.Value;
+            
+            // Key not in cache, so get data.
+            var cacheEntry = createItem();
+
+            Store(key, createItem(), expiresAfter ?? _defaultExpiresTime);
+
+            return cacheEntry;
+        }
+    }
+}
+
---
+++ tests/Rene.Utils.Core.UnitTest/SimpleCache/SimpleCacheEngineTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rene.Utils.Core.SimpleCache;
+
+namespace Rene.Utils.Core.UnitTest.SimpleCache
+{
+    public class SimpleCacheEngineTest : IDisposable
+    {
+        private SimpleCacheEngine<string, string> _cache;
+
+        public SimpleCacheEngineTest()
+        {
+            _cache= new SimpleCacheEngine<string,string>(TimeSpan.FromMilliseconds(10));
+        }
+
+
+        public void Dispose()
+        {
+        }
+    }
+}
