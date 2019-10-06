cb7ae290baf5f198efb5eb869023275ecb1c084a
René Pacios <rene15009@hotmail.com>
2019-10-06 03:04:08 +0200

    Add Dictionary Extensions

---
+++ src/Rene.Utils.Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,32 @@
+﻿
+
+// ReSharper disable once CheckNamespace
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        static DictionaryExtensions() { }
+
+        /// <summary>
+        /// Add item to dictionary if it not contains key 
+        /// </summary>
+        /// <typeparam name="K">Key Type</typeparam>
+        /// <typeparam name="T">Value Type</typeparam>
+        /// <param name="dictionary">Dictionary</param>
+        /// <param name="key">Key</param>
+        /// <param name="value">value</param>
+        /// <param name="override">[if key exist force update value]</param>
+        public static void AddIfNotExist<K, T>(this Dictionary<K, T> dictionary, K key, T value, bool @override = false)
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, value);
+                return;
+            }
+
+            if (!@override) return;
+
+            dictionary[key] = value;
+        }
+    }
+}
---
+++ tests/Rene.Utils.Core.UnitTest/Extensions/DictionaryExtensionsTest.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Rene.Utils.Core.UnitTest.Extensions
+{
+    public class DictionaryExtensionsTest
+    {
+        [Fact]
+        public void Add_Item_IfNotExist()
+        {
+
+            var dic = new Dictionary<int, int>();
+            int k = 1, v = 1;
+
+            dic.AddIfNotExist(k, v);
+
+            Assert.True(dic.ContainsKey(k));
+            Assert.Equal(dic[k], v);
+            Assert.Single(dic);
+        }
+
+        [Fact]
+        public void Add_ExistItem()
+        {
+            var dic = new Dictionary<int, int>();
+            int k = 1, v = 1, v2 = 2;
+
+            dic.Add(k, v);
+
+            dic.AddIfNotExist(k, v2);
+
+            Assert.True(dic.ContainsKey(k));
+            Assert.NotEqual(dic[k], v2);
+            Assert.Equal(dic[k], v);
+            Assert.Single(dic);
+
+            dic.AddIfNotExist(k, v2, true);
+
+            Assert.True(dic.ContainsKey(k));
+            Assert.NotEqual(dic[k], v);
+            Assert.Equal(dic[k], v2);
+            Assert.Single(dic);
+
+        }
+    }
+}
+
