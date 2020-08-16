c9ed71857d2525f65211769b9b03c9c1ee447280
René Pacios <rpacios@webrene.es>
2020-08-16 13:01:40 +0200

    Added enum helpers

---
+++ src/Rene.Utils.Core/Extensions/Enums/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿
+// ReSharper disable once CheckNamespace
+namespace System
+{
+
+    using ComponentModel;
+    using Reflection;
+
+    public static class EnumExtensions
+    {
+        public static string ToDisplayValue(this Enum value)
+        {
+            if (value==null) throw  new ArgumentNullException(nameof(value));
+
+            var field = value.GetType().GetField(value.ToString());
+            if (field == null) return value.ToString();
+           
+            var descrip = field.GetCustomAttribute<DescriptionAttribute>();
+            
+            return descrip?.Description ?? value.ToString();
+
+            
+        }
+    }
+}
