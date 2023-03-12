b6224aa632d7dfb81e9ddd8650b4f4911b9aca39
René Pacios <3912951+renepacios@users.noreply.github.com>
2023-03-12 10:00:58 +0100

    Add Enum Parse utility

--- src/Rene.Utils.Core/Resources/ExceptionMessages.Designer.cs
+++ src/Rene.Utils.Core/Resources/ExceptionMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace Rene.Utils.Core.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ExceptionMessages {
@@ -75,6 +75,15 @@ namespace Rene.Utils.Core.Resources {
         public static string NullReferenceMessage {
             get {
                 return ResourceManager.GetString("NullReferenceMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Input string can not be empty or null.
+        /// </summary>
+        public static string StringNullArgumentFormat {
+            get {
+                return ResourceManager.GetString("StringNullArgumentFormat", resourceCulture);
             }
         }
     }
