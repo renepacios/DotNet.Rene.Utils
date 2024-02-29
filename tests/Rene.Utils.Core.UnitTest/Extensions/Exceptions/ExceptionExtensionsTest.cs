31b6c7d4e0fe719570a4cb6a7eba14d6ba1f55bb
pabloff <pabloff@balidea.com>
2024-02-29 14:58:18 +0100

    Added class ExceptionExtensions with method GetFullMessage in order to all get exception stack trace and its tests

---
+++ tests/Rene.Utils.Core.UnitTest/Extensions/Exceptions/ExceptionExtensionsTest.cs
@@ -0,0 +1,82 @@
+﻿namespace Rene.Utils.Core.UnitTest.Extensions.Exceptions
+{
+    using FluentAssertions;
+    using System;
+    using Xunit;
+
+    public class ExceptionExtensionsTest
+    {
+        [Fact]
+        public void GetFullMessage_Should_Be_Exception_Message_When_No_InnerException()
+        {
+            // Arrange
+            var input = new Exception("Test Exception");
+
+            // Act
+            var result = input.GetFullMessage();
+
+            // Assert
+            result
+                .Should()
+                .NotBeNullOrEmpty()
+                .And
+                .BeEquivalentTo(input.Message)
+                ;
+        }
+
+        [Fact]
+        public void GetFullMessage_Should_Be_ExceptionPlusInner_Message_When_One_InnerException()
+        {
+            // Arrange
+            var innerException = new Exception("Inner Exception");
+            var input = new Exception("Test Exception", innerException);
+
+            // Act
+            var result = input.GetFullMessage();
+
+            // Assert
+            result
+                .Should()
+                .NotBeNullOrEmpty()
+                .And
+                .BeEquivalentTo($"{input.Message} ---> {innerException.Message}")
+                ;
+        }
+
+        [Fact]
+        public void GetFullMessage_Should_Be_ExceptionPlusAllInner_Message_When_Many_InnerException()
+        {
+            // Arrange
+            var innerException2 = new Exception("Inner Exception 2");
+            var innerException1 = new Exception("Inner Exception 1", innerException2);
+            var input = new Exception("Test Exception", innerException1);
+
+            // Act
+            var result = input.GetFullMessage();
+
+            // Assert
+            result
+                .Should()
+                .NotBeNullOrEmpty()
+                .And
+                .BeEquivalentTo($"{input.Message} ---> {innerException1.Message} ---> {innerException2.Message}")
+                ;
+        }
+
+        [Fact]
+        public void GetFullMessage_ShouldBe_Empty_When_Exception_IsNull()
+        {
+            // Arrange
+            Exception input = null;
+            
+            // Act
+            var result = input.GetFullMessage();
+
+            // Assert
+            result
+                .Should()
+                .BeNullOrEmpty()
+                ;
+        }
+    }
+}
